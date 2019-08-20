def351c427fa7ce0736810d785cb6df007347a5f
Silvia <sfracalossi@unibz.it>
2019-08-20 13:24:37 +0200

    Updated with windows commands

--- SparkComponent/commands.sh
+++ SparkComponent/commands.sh
@@ -1,8 +1,15 @@
-# first complie with
+# Path to this file
+# d:
+# cd \Uni\repositories\Programming Data Infrastructure\pdi2019-project\SparkComponent
+
+# Remove folders of out files
+# OS:
+# rm -rf ex_out
+# WINDOWS:
+rmdir ex_out
+
+# Compiles the project
 mvn package
 
-# Then remove folders of the previous run  (if it's not the first time)
-rm -rf ex_out
-
-# Run application
-spark-submit  --class SparkDriverLRegression --deploy-mode client --master local target/FlightsProject-1.0.0.jar "ex_data/trainingData.txt"  "ex_data/unlabeledData.txt" ex_out
+# Runs application
+spark-submit  --class SparkDriver --deploy-mode client --master local target/FlightsProject-1.0.0.jar
