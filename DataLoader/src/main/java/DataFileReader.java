6f59ac0c0d675f82b4ff54951b6f53858775777a
Silvia <sfracalossi@unibz.it>
2019-08-26 13:37:57 +0200

    Updated file reader - now unifies multiple files

--- DataLoader/src/main/java/DataFileReader.java
+++ DataLoader/src/main/java/DataFileReader.java
@@ -5,29 +5,53 @@ import java.util.*;
 public class DataFileReader {
   static String parsed_data_path = "../parsed_data/";
 
-	public List<String> ParseFile(String entity) {
-		String file = parsed_data_path+entity+"/part-00000";
+	public List<String> ParseFile(String entity, Boolean has_header) {
+		String directory_path = parsed_data_path+entity+"/part-";
 
+		int i = 0;
+		String file_number = "";
 		String row;
 		List<String> list = new ArrayList<String>();
 
 			try {
 
-				// Reading the file
-				BufferedReader br = new BufferedReader(new FileReader(file));
+				// Preparing the number of the file, starting from part-00000
+				file_number = i + "";
+				while (file_number.length() != 5) {
+					file_number = "0" + file_number;
+				}
 
-				// Removing the header of the file
-				String header = br.readLine();
-				System.out.println("[INFO] Parsing file " +file);
-				System.out.println("[INFO] Header of file is " +header);
+				// Checking if exists file
+				String file_path = directory_path + file_number;
+				while (new File(file_path).exists()) {
 
-				// Transforming the content of the file into a List<String>
-				while ((row = br.readLine()) != null) {
-					list.add(row);
+					//Reading file
+					System.out.println("[INFO] Parsing file " +file_path);
+					BufferedReader file_br = new BufferedReader(new FileReader(file_path));
+
+					// Removing the header of the file
+					if (has_header) {
+						String header = file_br.readLine();
+						System.out.println("[INFO] Header of file is " +header);
+					}
+
+					// Reading each line of the file
+					while ((row = file_br.readLine()) != null) {
+						list.add(row);
+					}
+					file_br.close();
+
+					// Preparing the number of the file
+					i = i + 1;
+					file_number = i + "";
+					while (file_number.length() != 5) {
+						file_number = "0" + file_number;
+					}
+					file_path = directory_path + file_number;
 				}
 
 			} catch (Exception e) {
-				System.out.println("[ERROR] Error with the airlines parsing");
+				System.out.println("[ERROR] Error with the parsing of " + entity);
 				System.out.println(e);
 			}
 
