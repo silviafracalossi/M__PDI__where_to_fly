8d1723c873bb789579732faa9d57450f917d06c6
Silvia <sfracalossi@unibz.it>
2019-08-30 22:41:44 +0200

    Added analytics page in HTML

--- DataLoader/src/main/java/Main.java
+++ DataLoader/src/main/java/Main.java
@@ -1,16 +1,27 @@
 
 import java.awt.EventQueue;
+import java.util.List;
+import org.hibernate.Session;
 
 public class Main {
 
 	public static void main(String[] args) {
 
 		// Creating the connection with the db
-		//DatabaseConnector db_connector = new DatabaseConnector();
-		//Session db_session = db_connector.session;
+		DatabaseConnector db_connector = new DatabaseConnector();
+		Session db_session = db_connector.session;
 
+		// Getting all the analytics
+		AnalyticsServiceMysql asm = new AnalyticsServiceMysql(db_session);
+		asm.generateVisualization();
+        
+
+		System.exit(0);
+        
+		
+		
 		// Initialize the interface
-		EventQueue.invokeLater(new Runnable() {
+		/*EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					LoginWindow window = new LoginWindow();
@@ -18,8 +29,7 @@ public class Main {
 					e.printStackTrace();
 				}
 			}
-		});
-		
+		});*/
 	}
 
 }
